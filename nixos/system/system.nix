09a30ac46a46b013d809daaa3dea3e7e4ae08190
Elias Schlosser <eljangus@gmail.com>
2026-07-27 18:04:04 +0200

    ig we don't test nixpkgs for breakages anymore?? niri overlay added...

--- nixos/system/system.nix
+++ nixos/system/system.nix
@@ -111,6 +111,20 @@
       (final: prev: {
         swash = inputs.swash.packages.${pkgs.stdenv.hostPlatform.system}.default;
       })
+      (final: prev: {
+        niri = prev.niri.override {
+          libdisplay-info = prev.libdisplay-info.overrideAttrs (finalAttrs: {
+            version = "0.3.0";
+            src = prev.fetchFromGitLab {
+              domain = "gitlab.freedesktop.org";
+              owner = "emersion";
+              repo = "libdisplay-info";
+              rev = finalAttrs.version;
+              sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+            };
+          });
+        };
+      })
     ];
     config = {
       permittedInsecurePackages = [
