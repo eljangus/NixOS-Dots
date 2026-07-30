21037a48ff8d08ce8730eeee76791c25530a0c4d
Elias Schlosser <eljangus@gmail.com>
2026-07-31 01:02:05 +0200

    improved lib

--- nixos/lib/import-tree.nix
+++ nixos/lib/import-tree.nix
@@ -4,8 +4,11 @@ dir:
 let
   inherit (builtins) filter map baseNameOf;
   inherit (lib.filesystem) listFilesRecursive;
-  inherit (lib.strings) hasSuffix;
+  inherit (lib.strings) hasSuffix hasPrefix;
 
   files = map toString (listFilesRecursive dir);
 in
-filter (p: hasSuffix ".nix" p && baseNameOf p != "default.nix") files
+filter (
+  p:
+  hasSuffix ".nix" p && baseNameOf p != "default.nix" && !(hasPrefix "_" (baseNameOf p))
+) files
