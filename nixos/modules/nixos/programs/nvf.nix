1317aa142fc938cfc99c01e08cc74ca8d13c8e69
Elias Schlosser <eljangus@gmail.com>
2026-08-04 17:20:36 +0200

    fixed dashboard alpha

--- nixos/modules/nixos/programs/nvf.nix
+++ nixos/modules/nixos/programs/nvf.nix
@@ -14,8 +14,8 @@
 
           theme = {
             enable = true;
-            name = "tokyonight";
-            style = "storm";
+            name = "onedark";
+            style = "dark";
             transparent = false;
           };
 
@@ -43,6 +43,202 @@
           # UI / QoL
           statusline.lualine.enable = true;
           tabline.nvimBufferline.enable = true;
+          dashboard.alpha = {
+            enable = true;
+            # Must be null: nvf's alpha module defaults `theme` to "dashboard", and it
+            # asserts that a built-in theme and a custom layout are never set together.
+            theme = null;
+            layout = [
+              {
+                type = "padding";
+                val = 4;
+              }
+              {
+                type = "text";
+                val = [
+                  "  ███╗   ██╗██╗   ██╗██╗███╗   ███╗ "
+                  "  ████╗  ██║██║   ██║██║████╗ ████║ "
+                  "  ██╔██╗ ██║██║   ██║██║██╔████╔██║ "
+                  "  ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║ "
+                  "  ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║ "
+                  "  ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝ "
+                ];
+                opts = {
+                  position = "center";
+                  hl = "String";
+                };
+              }
+              {
+                type = "padding";
+                val = 2;
+              }
+              {
+                type = "group";
+                val = [
+                  {
+                    type = "button";
+                    val = "  Neue Datei";
+                    on_press = lib.generators.mkLuaInline "function() vim.cmd('ene | startinsert') end";
+                    opts = {
+                      position = "center";
+                      shortcut = "e";
+                      cursor = 3;
+                      width = 42;
+                      align_shortcut = "right";
+                      hl = "Type";
+                      hl_shortcut = "Keyword";
+                      keymap = [
+                        "n"
+                        "e"
+                        ":ene <BAR> startinsert <CR>"
+                        {
+                          noremap = true;
+                          silent = true;
+                          nowait = true;
+                        }
+                      ];
+                    };
+                  }
+                  {
+                    type = "button";
+                    val = "  Datei suchen";
+                    on_press = lib.generators.mkLuaInline "function() require('telescope.builtin').find_files() end";
+                    opts = {
+                      position = "center";
+                      shortcut = "f";
+                      cursor = 3;
+                      width = 42;
+                      align_shortcut = "right";
+                      hl = "Type";
+                      hl_shortcut = "Keyword";
+                      keymap = [
+                        "n"
+                        "f"
+                        ":lua require('telescope.builtin').find_files()<CR>"
+                        {
+                          noremap = true;
+                          silent = true;
+                          nowait = true;
+                        }
+                      ];
+                    };
+                  }
+                  {
+                    type = "button";
+                    val = "  Wort suchen";
+                    on_press = lib.generators.mkLuaInline "function() require('telescope.builtin').live_grep() end";
+                    opts = {
+                      position = "center";
+                      shortcut = "w";
+                      cursor = 3;
+                      width = 42;
+                      align_shortcut = "right";
+                      hl = "Type";
+                      hl_shortcut = "Keyword";
+                      keymap = [
+                        "n"
+                        "w"
+                        ":lua require('telescope.builtin').live_grep()<CR>"
+                        {
+                          noremap = true;
+                          silent = true;
+                          nowait = true;
+                        }
+                      ];
+                    };
+                  }
+                  {
+                    type = "button";
+                    val = "  Zuletzt geöffnet";
+                    on_press = lib.generators.mkLuaInline "function() require('telescope.builtin').oldfiles() end";
+                    opts = {
+                      position = "center";
+                      shortcut = "r";
+                      cursor = 3;
+                      width = 42;
+                      align_shortcut = "right";
+                      hl = "Type";
+                      hl_shortcut = "Keyword";
+                      keymap = [
+                        "n"
+                        "r"
+                        ":lua require('telescope.builtin').oldfiles()<CR>"
+                        {
+                          noremap = true;
+                          silent = true;
+                          nowait = true;
+                        }
+                      ];
+                    };
+                  }
+                  {
+                    type = "button";
+                    val = "  Config bearbeiten";
+                    on_press = lib.generators.mkLuaInline "function() vim.cmd('cd ~/NixOS-Dots/nixos'); require('telescope.builtin').find_files({ hidden = true, no_ignore = true, follow = true }) end";
+                    opts = {
+                      position = "center";
+                      shortcut = "c";
+                      cursor = 3;
+                      width = 42;
+                      align_shortcut = "right";
+                      hl = "Type";
+                      hl_shortcut = "Keyword";
+                      keymap = [
+                        "n"
+                        "c"
+                        ":lua vim.cmd('cd ~/NixOS-Dots/nixos'); require('telescope.builtin').find_files({ hidden = true, no_ignore = true, follow = true })<CR>"
+                        {
+                          noremap = true;
+                          silent = true;
+                          nowait = true;
+                        }
+                      ];
+                    };
+                  }
+                  {
+                    type = "button";
+                    val = "  Beenden";
+                    on_press = lib.generators.mkLuaInline "function() vim.cmd('qa') end";
+                    opts = {
+                      position = "center";
+                      shortcut = "q";
+                      cursor = 3;
+                      width = 42;
+                      align_shortcut = "right";
+                      hl = "Type";
+                      hl_shortcut = "Keyword";
+                      keymap = [
+                        "n"
+                        "q"
+                        ":qa<CR>"
+                        {
+                          noremap = true;
+                          silent = true;
+                          nowait = true;
+                        }
+                      ];
+                    };
+                  }
+                ];
+                opts = {spacing = 1;};
+              }
+              {
+                type = "padding";
+                val = 2;
+              }
+              {
+                type = "text";
+                val = lib.generators.mkLuaInline "os.date('  %A, %d.%m.%Y  •  %H:%M')";
+                opts = {
+                  position = "center";
+                  hl = "Comment";
+                };
+              }
+            ];
+            opts = {
+              margin = 5;
+            };
+          };
 
           visuals = {
             nvim-web-devicons.enable = true;
@@ -53,25 +249,26 @@
           binds.whichKey.enable = true; # keybind popup helper
           autopairs.nvim-autopairs.enable = true;
 
-          # File explorer + dashboard
-          filetree.nvimTree = {
-            enable = true;
-            mappings.toggle = "<leader>e";
-            view = {
-              width = {
-                min = 25;
-                max = 30;
-                padding = 1;
-              };
-            };
-          };
-
           # Comments / TODOs
           comments.comment-nvim.enable = true;
           notes.todo-comments.enable = true;
 
           # Navigation
-          telescope.enable = true;
+          telescope = {
+            enable = true;
+            setupOpts = {
+              pickers = {
+                find_files = {
+                  find_command = [
+                    "${pkgs.fd}/bin/fd"
+                    "--type=file"
+                    "--hidden"
+                    "--no-ignore"
+                  ];
+                };
+              };
+            };
+          };
 
           # Git
           git.gitsigns.enable = true;
