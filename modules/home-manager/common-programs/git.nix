49ab11619da464c18927c4a2c86c700bc865dd1e
Elias Schlosser <eljangus@gmail.com>
2026-08-05 17:50:57 +0200

    updated git hm config

--- modules/home-manager/common-programs/git.nix
+++ modules/home-manager/common-programs/git.nix
@@ -3,41 +3,10 @@
     enable = true;
 
     userName = "Elias Schlosser";
-    userEmail = "CHANGE_ME@example.com";
-
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
-        side-by-side = false;
-      };
-    };
-
-    aliases = {
-      s = "status --short --branch";
-      l = "log --oneline --graph --decorate -20";
-      la = "log --oneline --graph --decorate --all";
-      last = "log -1 HEAD --stat";
-      unstage = "restore --staged";
-      amend = "commit --amend --no-edit";
-    };
-
-    ignores = [
-      "result"
-      "result-*"
-      ".direnv/"
-      ".DS_Store"
-    ];
+    userEmail = "eljangus@example.com";
 
     extraConfig = {
       init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      pull.rebase = true;
-      rebase.autoStash = true;
-      fetch.prune = true;
-      merge.conflictStyle = "zdiff3";
-      diff.algorithm = "histogram";
       core.editor = "vim";
     };
   };
