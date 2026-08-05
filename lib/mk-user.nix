619093051536d894c731eab5cc2ba4f64729af69
Elias Schlosser <eljangus@gmail.com>
2026-08-05 16:16:05 +0200

    huge refactor, seperated dotfiles from nixos stuff

---
+++ lib/mk-user.nix
@@ -0,0 +1,73 @@
+{
+  name,
+  host,
+  description ? "Elias Schlosser",
+  stateVersion ? "26.05",
+  extraGroups ? [
+    "networkmanager"
+    "wheel"
+  ],
+  hmModules ? [],
+}: {
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}: let
+  isDarwin = config.nixpkgs.hostPlatform.isDarwin;
+
+  homeDirectory =
+    if isDarwin
+    then "/Users/${name}"
+    else "/home/${name}";
+
+  nhPlatform =
+    if isDarwin
+    then "darwin"
+    else "os";
+
+  nhStage =
+    if isDarwin
+    then "build"
+    else "boot";
+
+  flakeRef = "~/nixos#${host}";
+in {
+  users.users.${name} =
+    {
+      inherit name description;
+      home = homeDirectory;
+      shell = pkgs.fish;
+    }
+    // lib.optionalAttrs (!isDarwin) {
+      isNormalUser = true;
+      inherit extraGroups;
+      hashedPasswordFile = "/etc/nixos/secrets/${name}.txt";
+    };
+
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    backupFileExtension = "backup";
+
+    users.${name} = {
+      imports =
+        [(self + "/modules/home-manager/common-programs")]
+        ++ hmModules;
+
+      home = {
+        inherit stateVersion homeDirectory;
+        username = name;
+        sessionVariables = {
+          EDITOR = "vim";
+        };
+      };
+
+      programs.fish.shellAliases = {
+        nrs = "nh ${nhPlatform} switch ${flakeRef}";
+        nrb = "nh ${nhPlatform} ${nhStage} ${flakeRef}";
+      };
+    };
+  };
+}
---
+++ modules/home-manager/common-programs/fish.nix
@@ -0,0 +1,47 @@
+{...}: {
+  programs = {
+    fish = {
+      enable = true;
+      functions = {
+        ps5 = ''
+          set battery_paths /sys/class/power_supply/ps-controller-battery-*
+          if not set -q battery_paths[1]
+            echo "🎮 PS5 Controller: nicht verbunden"
+            return 1
+          end
+
+          set battery_path $battery_paths[1]
+          set capacity (cat $battery_path/capacity)
+          set battery_status (cat $battery_path/status)
+
+          # Simple bar
+          set bar_length 20
+          set filled (math "round($capacity / 100 * $bar_length)")
+          set empty (math "$bar_length - $filled")
+
+          set bar (string repeat -n $filled '█')(string repeat -n $empty '░')
+
+          echo "🎮 PS5 Controller: [$bar] $capacity% ($battery_status)"
+        '';
+      };
+      shellInit = ''
+        set -U fish_greeting ""
+      '';
+      shellAliases = {
+        ncg = "nh clean all";
+        lg = "lazygit";
+        cff = "reset && fastfetch";
+        cff2 = "reset && fastfetch -c ~/.config/fastfetch/other-configs/nitch-like/config.jsonc";
+        cff3 = "reset && fastfetch -c ~/.config/fastfetch/other-configs/Schlosser/config.jsonc";
+        cff4 = "reset && fastfetch -c ~/.config/fastfetch/other-configs/kitty/config.jsonc";
+        cff5 = "reset && fastfetch -c ~/.config/fastfetch/other-configs/frieren/config.jsonc";
+        cff6 = "reset && python3 ~/dotfiles/misc/misc-text/bunnyfetch.py";
+        ff = "fastfetch";
+        ff2 = "fastfetch -c ~/.config/fastfetch/other-configs/nitch-like/config.jsonc";
+        ff3 = "fastfetch -c ~/.config/fastfetch/other-configs/Schlosser/config.jsonc";
+        ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
+        ls = "eza -la --icons";
+      };
+    };
+  };
+}
